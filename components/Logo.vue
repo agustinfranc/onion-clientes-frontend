93f1bb552f5d2ce7324ea882bc83692c93700bf5
Agustin Amato <agustin.amato96@gmail.com>
2020-11-23 23:29:05 -0300

    lint fixes

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <v-img
-      max-height="150"
-      max-width="200"
-      src="/logo.png"
-    ></v-img>
+    <v-img max-height="150" max-width="200" src="/logo.png"></v-img>
   </div>
 </template>
