488bd3d4131448c65e1576c489b5c1d43c913c31
Agustin Amato <agustin.amato96@gmail.com>
2021-04-11 22:47:08 -0300

    BUGFIX se corrigió bug de portada y avatar de comercio en dashboard; si cambiabas de comercio, siempre quedaba la portada del primer comercio.

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
           {{ $t('dashboard.commerce.title') }}
         </v-card-title>
         <v-card-text>
-          <FormsCommerceForm />
+          <FormsCommerceForm v-if="!$fetchState.pending" />
         </v-card-text>
       </v-card>
     </v-col>
@@ -49,7 +49,7 @@
           <div>
             <v-data-table
               :headers="headers"
-              :items="body"
+              :items="products"
               :items-per-page="5"
               item-key="name"
               class="elevation-1"
@@ -122,17 +122,19 @@ export default {
           value: 'disabled',
         },
       ],
-      body: [],
+      products: [],
       loading: true,
     }
   },
 
   async fetch() {
     try {
-      const url = `api/auth/commerces/${this.$store.state.commerce.id}/products`
+      let url = `api/auth/commerces/${this.$store.state.commerce.id}`
       const res = await this.$nuxt.$axios.$get(url)
+      this.$store.dispatch('saveCommerce', res)
 
-      this.body = res
+      url = `api/auth/commerces/${this.$store.state.commerce.id}/products`
+      this.products = await this.$nuxt.$axios.$get(url)
     } catch (error) {
       this.toggleSnackbar({ text: 'Ocurrió un error', color: 'red accent-4' })
     } finally {
