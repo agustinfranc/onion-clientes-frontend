5c7e4c199d53bf5dbab27bd4c3c0b5100eee602a
Agustin Amato <agustin.amato96@gmail.com>
2024-09-27 01:52:42 -0300

    chore: make app static

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ export default {
   },
 
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: true,
+  ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
+  target: 'static',
 
   /*
    ** Environment Variables
